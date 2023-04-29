6f2f2436cdc93b4430011b3ff2705717771b2abb
luccamicale <luccamicale2015@gmail.com>
2023-04-28 23:33:39 -0300

    kkll

--- src/components/shoppingCartproduct.jsx
+++ src/components/shoppingCartproduct.jsx
@@ -4,7 +4,7 @@ const ShoppingCartProduct = ({data, deleteFromCart}) => {
     return (
         <div>
             <h2>{data.name}</h2>
-            <p>Price: {data.price}</p>
+            <p>Price= {data.price}</p>
             <button onClick={() => deleteFromCart(data.id)}>Delete Fromm Cart</button>
         </div>
     )
